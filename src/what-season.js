5bcbdbbcfaa1b65344d7d426903c470cdc42c7b8
Lina Gorihvostka <lina.editor@gmail.com>
2021-03-27 21:35:29 +0300

    fix: fix incorrect or absent argument handling

--- src/what-season.js
+++ src/what-season.js
@@ -1,30 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(dateSample) {
-	let month = dateSample.getMonth() + 1; // 1 — 12
-	switch (month) {
-		case 12:
-		case 1:
-		case 2:
-			return "winter";
-			break;
-		case 3:
-		case 4:
-		case 5:
-			return "spring";
-			break;
-		case 6:
-		case 7:
-		case 8:
-			return "summer";
-			break;
-		case 9:
-		case 10:
-		case 11:
-			return "autumn";
-			break;
-		default:
-			return Error;
-			break;
-	}
+  if (!dateSample) {
+    return 'Unable to determine the time of year!';
+  } else if (dateSample instanceof Date && !isNaN(dateSample)) {
+    let month = dateSample.getMonth() + 1; // 1 — 12
+    switch (month) {
+      case 12:
+      case 1:
+      case 2:
+        return "winter";
+        break;
+      case 3:
+      case 4:
+      case 5:
+        return "spring";
+        break;
+      case 6:
+      case 7:
+      case 8:
+        return "summer";
+        break;
+      case 9:
+      case 10:
+      case 11:
+        return "autumn";
+        break;
+    }
+  } else {
+    throw new Error();
+  }
 };
