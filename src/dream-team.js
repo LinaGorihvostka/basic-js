a84a22f8ba54f0ca2672cdcdc4ce7ad195250e78
Lina Gorihvostka <lina.editor@gmail.com>
2021-03-27 14:44:27 +0300

    fix: return false on wrong type

--- src/dream-team.js
+++ src/dream-team.js
@@ -5,13 +5,17 @@ module.exports = function createDreamTeam(members) {
   let currentName = '';
   let formattedName = ''
   let firstLetter = '';
-  for (i = 0; i < members.length; i++) {
-    if (typeof members[i] == 'string') {
-      currentName = members[i];
-      formattedName = currentName.trim().toUpperCase().replace(' ', '');
-      firstLetter = formattedName[0];
-      dreamTeam.push(firstLetter);
+  if (Array.isArray(members)) {
+    for (i = 0; i < members.length; i++) {
+      if (typeof members[i] == 'string') {
+        currentName = members[i];
+        formattedName = currentName.trim().toUpperCase().replace(' ', '');
+        firstLetter = formattedName[0];
+        dreamTeam.push(firstLetter);
+      }
     }
+    return dreamTeam.sort().join('');
+  } else {
+    return false;
   }
-  return dreamTeam.sort().join('');
 };
