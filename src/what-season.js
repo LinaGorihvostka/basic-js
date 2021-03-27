edb682422052cf5a6e00ae99c762ca8b0616d5ed
Lina Gorihvostka <lina.editor@gmail.com>
2021-03-27 15:27:10 +0300

    init: start what season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(dateSample) {
+	let month = dateSample.getMonth() + 1; // 1 — 12
+	switch (month) {
+		case 12:
+		case 1:
+		case 2:
+			return "winter";
+			break;
+		case 3:
+		case 4:
+		case 5:
+			return "spring";
+			break;
+		case 6:
+		case 7:
+		case 8:
+			return "summer";
+			break;
+		case 9:
+		case 10:
+		case 11:
+			return "autumn";
+			break;
+		default:
+			return Error;
+			break;
+	}
 };
