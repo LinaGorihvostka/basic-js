6b4dbe6109d8c6f4dd0e09f4d4b8e6c7c8954cdc
Lina Gorihvostka <lina.editor@gmail.com>
2021-03-29 15:03:23 +0300

    init: solve transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(array) {
+  if (!Array.isArray(array)) {
+    throw new Error();
+  }
+  if (!array.length) {
+    return array;
+  }
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+
+    switch (array[i]) {
+      case '--discard-next':
+        if (array[i + 2] == '--discard-prev' ||
+        array[i + 2] == '--double-prev') {
+          i = i + 2;
+        } else {
+        i++;
+        }
+        break;
+      case '--double-next':
+        if (i != array.length - 1) {
+          newArray.push(array[i + 1]);
+        }
+        break;
+      case '--discard-prev':
+        if (i != 0) {
+          newArray.pop();
+        }
+        break;
+      case '--double-prev':
+        if (i != 0) {
+          newArray.push(array[i - 1]);
+        }
+        break;
+      default:
+        newArray.push(array[i]);
+        break;
+    }
+  }
+  return newArray;
 };
